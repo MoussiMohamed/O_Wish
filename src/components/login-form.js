993268bbebea6115113aef34057d8324bb9a9751
MohamedMSI <msi.moussi@gmail.com>
2017-03-25 14:11:00 +0100

    CSS enhancements for login form.

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -10,7 +10,6 @@ export default class LoginForm extends React.Component {
   render() {
     return (
       <form className="login-form" onSubmit={this._handleLogin.bind(this)}>
-        <label>New wish</label>
         <div className="login-form-fields">
           <input type="email" name="email" placeholder="Username:" ref={c => this._email = c}/>
           <input type="password" name="password" placeholder="Password:" ref={c => this._password = c}/>
